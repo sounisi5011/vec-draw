c2020983c8a8f0215ae26a0dd865cb20794cdb5a
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-01-22 00:36:00 +0900

    ✨ Git Hookのデバッグ情報でファイルの内容を表示する
    scripts/hook-debug.jsスクリプトに、Git Hookの引数に含まれているファイルパスの内容を表示する処理を追加した。

--- scripts/hook-debug.js
+++ scripts/hook-debug.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const fs = require('fs');
 const path = require('path');
 const indent = require('./src/indent.js');
 
@@ -14,5 +15,21 @@ console.log(indent([
       `env.${name}:\n` +
       indent(JSON.stringify(process.env[name], null, 2))
     )),
+
+  /*
+   * git hookの引数にファイルパスが含まれている場合は、ファイルの内容を表示する
+   */
+  ...(process.env.HUSKY_GIT_PARAMS || '').split(/ +/)
+    .map(param => {
+      try {
+        if (param !== '') {
+          const filePath = path.resolve(process.cwd(), param);
+          const fileStr = fs.readFileSync(filePath, { encoding: 'utf-8' });
+          return `file:./${param}:\n${fileStr.replace(/^/mg, '  > ')}`;
+        }
+      } catch(err) {}
+      return null;
+    })
+    .filter(data => data !== null),
   '',
 ].join('\n'), 4));
