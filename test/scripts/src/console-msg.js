4c004d281a7bff7dee2fdc45195fc57398b40503
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-01-27 20:18:33 +0900

    ✅ scripts/src/console-msg.jsのテストコードを追加
    汎用関数 consoleMsg() の動作テストを行うテストコードを追加。

---
+++ test/scripts/src/console-msg.js
@@ -0,0 +1,70 @@
+const test = require('ava');
+const consoleMsg = require('../../../scripts/src/console-msg.js');
+
+test('consoleMsg() / 先頭行以降は字数に合わせてインデント', t => {
+  const inputLines =
+    'line1\n' + //
+    'line2\n' +
+    'line3';
+  const outputLines =
+    'output: line1\n' + //
+    '        line2\n' +
+    '        line3';
+
+  t.is(consoleMsg('output: ', inputLines), outputLines);
+});
+
+test('consoleMsg() / 改行コードは維持', t => {
+  const inputLines =
+    'line1\r' + //
+    'line2\n' +
+    'line3\r\n' +
+    'line4';
+  const outputLines =
+    'test: line1\r' + //
+    '      line2\n' +
+    '      line3\r\n' +
+    '      line4';
+
+  t.is(consoleMsg('test: ', inputLines), outputLines);
+});
+
+test('consoleMsg() / 空行はインデントしない', t => {
+  const inputLines =
+    '42\n' +
+    '\n' +
+    '42\r' +
+    '\r' +
+    '\r' +
+    '42\r\n' +
+    '\r\n' +
+    '\r\n' +
+    '\r\n' +
+    '42\n' +
+    '\n' +
+    '\n' +
+    '\n' +
+    '\n' +
+    '\n';
+  const outputLines =
+    'The answer to life the universe and everything: 42\n' +
+    '\n' +
+    '                                                42\r' +
+    '\r' +
+    '\r' +
+    '                                                42\r\n' +
+    '\r\n' +
+    '\r\n' +
+    '\r\n' +
+    '                                                42\n' +
+    '\n' +
+    '\n' +
+    '\n' +
+    '\n' +
+    '\n';
+
+  t.is(
+    consoleMsg('The answer to life the universe and everything: ', inputLines),
+    outputLines,
+  );
+});
