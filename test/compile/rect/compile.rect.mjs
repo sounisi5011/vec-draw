aae81d27ce33ff7c29404da24041c39dc39980cc
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-01-28 23:58:52 +0900

    ✅ 複数のrect文を変換するテストを追加
    複数存在するrect文をコンパイルし、rect要素を適切に生成できるかを検証するテストコードを作成した。

--- test/compile/rect/compile.rect.mjs
+++ test/compile/rect/compile.rect.mjs
@@ -15,3 +15,10 @@ test('単一のrect文の変換', async t => {
 
   t.is(compile(srcText), destText);
 });
+
+test('複数のrect文の変換', async t => {
+  const srcText = await readFileAsync('multi-rect.vec-draw', 'utf-8');
+  const destText = await readFileAsync('multi-rect.svg', 'utf-8');
+
+  t.is(compile(srcText), destText);
+});
