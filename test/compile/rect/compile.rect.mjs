cf96b14afc131dba958031ca84bef925d1af70d1
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-01-28 23:58:52 +0900

    ✅ rect文を変換するテストを追加
    rect文をコンパイルし、rect要素が入ったSVGに変換できるかを検証するテストコードを作成した。

---
+++ test/compile/rect/compile.rect.mjs
@@ -0,0 +1,17 @@
+import test from 'ava';
+import { readFile } from 'fs';
+import { promisify } from 'util';
+import { compile } from '../../../src/index';
+
+const readFileAsync = promisify(readFile);
+
+test.before(() => {
+  process.chdir('./test/compile/rect/');
+});
+
+test('単一のrect文の変換', async t => {
+  const srcText = await readFileAsync('single-rect.vec-draw', 'utf-8');
+  const destText = await readFileAsync('single-rect.svg', 'utf-8');
+
+  t.is(compile(srcText), destText);
+});
