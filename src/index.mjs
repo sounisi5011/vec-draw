07fbc2e4f4153a16bf871434a20c51d7e73a87a7
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-01-29 01:54:37 +0900

    ✨ compile関数の結果をVNodeで生成するように変更
    コンパイル結果を、Virtual Nodeを使用して生成するように変更。

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,39 @@
 /**
+ * @typedef {{nodeName: string, attributes: Object<string, string>, children: Array<VNode>}} VNode
+ */
+
+/**
+ * @param {VNode} vnode
+ * @return {string}
+ */
+function vnode2str({ nodeName, attributes, children }) {
+  const attrsStr = Object.entries(attributes)
+    .map(([attr, value]) => ` ${attr}="${value}"`)
+    .join('');
+
+  if (children.length > 0) {
+    const childrenStr = children
+      .map(vnode2str)
+      .join('')
+      .replace(/^(?=[^\r\n]+$)/gm, '  ');
+    return `<${nodeName}${attrsStr}>\n${childrenStr}</${nodeName}>\n`;
+  }
+
+  return `<${nodeName}${attrsStr}/>\n`;
+}
+
+/**
  * @param {string} text 変換するvec-draw DSLの文字列
  * @return {string} 生成したSVG
  */
 export function compile(text) {
-  if (text === '') {
-    return '<svg xmlns="http://www.w3.org/2000/svg"></svg>';
-  }
-};
+  const rootElem = {
+    nodeName: 'svg',
+    attributes: {
+      version: '1.1',
+      xmlns: 'http://www.w3.org/2000/svg',
+    },
+    children: [],
+  };
+  return vnode2str(rootElem);
+}
