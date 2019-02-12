ae011be164b66108cbeb7c9f889daf5caaf77802
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-02-12 23:00:19 +0900

    ✨ XMLErrorクラスを追加
    DSLに含まれるXMLのシンタックスに関するエラーのクラス XMLError を作成した。

--- src/error/index.mjs
+++ src/error/index.mjs
@@ -1,3 +1,4 @@
 export { default as BaseError } from './base';
 export { default as IndentationError } from './indentation';
 export { default as SyntaxError } from './syntax';
+export { default as XMLError } from './xml';
