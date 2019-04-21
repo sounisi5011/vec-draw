e050ad231ebd6fca0752b08f9f7bc60207920001
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-04-21 22:50:50 +0900

    🏷️ PEG.jsのAPIに関する型情報を、名前空間 PEGJs 内に定義
    PEG.jsが自動生成するAPIに関する型情報を名前空間で分離した。

--- src/parser/dsl.pegjs.d.ts
+++ src/parser/dsl.pegjs.d.ts
@@ -1,66 +1,68 @@
-interface ParserOptions {
-    startRule?: string;
+export namespace PEGJs {
+    export interface ParserOptions {
+        startRule?: string;
+    }
+
+    export interface LiteralExpectation {
+        type: 'literal';
+        text: string;
+        ignoreCase: boolean;
+    }
+
+    export interface ClassExpectation {
+        type: 'class';
+        parts: ([string, string] | string)[];
+        inverted: boolean;
+        ignoreCase: boolean;
+    }
+
+    export interface AnyExpectation {
+        type: 'any';
+    }
+
+    export interface EndExpectation {
+        type: 'end';
+    }
+
+    export interface OtherExpectation {
+        type: 'other';
+        description: string | unknown;
+        // Note: descriptionパラメータの値は、PEG.jsの実装上はstringを想定しているように思えるが、
+        //       expected()関数の引数にどのような値も渡せるため、
+        //       unknown型も併記した。
+    }
+
+    export type Expectation =
+        | LiteralExpectation
+        | ClassExpectation
+        | AnyExpectation
+        | EndExpectation
+        | OtherExpectation;
+
+    export interface Location {
+        start: {
+            offset: number;
+            line: number;
+            column: number;
+        };
+        end: {
+            offset: number;
+            line: number;
+            column: number;
+        };
+    }
 }
 
-export interface LiteralExpectation {
-    type: 'literal';
-    text: string;
-    ignoreCase: boolean;
-}
-
-export interface ClassExpectation {
-    type: 'class';
-    parts: ([string, string] | string)[];
-    inverted: boolean;
-    ignoreCase: boolean;
-}
-
-export interface AnyExpectation {
-    type: 'any';
-}
-
-export interface EndExpectation {
-    type: 'end';
-}
-
-export interface OtherExpectation {
-    type: 'other';
-    description: string | unknown;
-    // Note: descriptionパラメータの値は、PEG.jsの実装上はstringを想定しているように思えるが、
-    //       expected()関数の引数にどのような値も渡せるため、
-    //       unknown型も併記した。
-}
-
-export type Expectation =
-    | LiteralExpectation
-    | ClassExpectation
-    | AnyExpectation
-    | EndExpectation
-    | OtherExpectation;
-
-interface Location {
-    start: {
-        offset: number;
-        line: number;
-        column: number;
-    };
-    end: {
-        offset: number;
-        line: number;
-        column: number;
-    };
-}
-
-export function parse(input: string, options?: ParserOptions): unknown;
+export function parse(input: string, options?: PEGJs.ParserOptions): unknown;
 
 export class SyntaxError extends Error {
     public message: string;
 
-    public expected: Expectation[] | null;
+    public expected: PEGJs.Expectation[] | null;
 
     public found: string | null;
 
-    public location: Location;
+    public location: PEGJs.Location;
 
     public name: 'SyntaxError';
 }
