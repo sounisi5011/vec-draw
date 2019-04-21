4cc62ed57c445ef7b5596f5afba73ccfd2d977cd
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-04-22 00:03:33 +0900

    🏷️ PEG.jsが生成するパーサが返すASTの型情報を追加
    PEG.jsが生成するパーサ dsl.pegjs.js が結果として返してくるASTオブジェクトの型情報を追加した。

--- src/parser/dsl.pegjs.d.ts
+++ src/parser/dsl.pegjs.d.ts
@@ -53,7 +53,120 @@ export namespace PEGJs {
     }
 }
 
-export function parse(input: string, options?: PEGJs.ParserOptions): unknown;
+export namespace AST {
+    export interface Node {
+        type: string;
+        position: PEGJs.Location;
+    }
+
+    export interface Parent extends Node {
+        children: Node[];
+    }
+
+    export interface Literal extends Node {
+        value: unknown;
+    }
+
+    export interface StatementNode extends Parent {
+        type: 'statement';
+        name: string;
+        nameSymbol: SymbolNode;
+        attributes: { [key: string]: ValueNode };
+        attributeNodes: AttributeNode[];
+        children: Exclude<StatementValueNode, AttributeNode | CommentNode>[];
+        fullChildren: StatementValueNode[];
+    }
+
+    export type StatementValueNode =
+        | CommentNode
+        | XMLNode
+        | AttributeNode
+        | StatementNode
+        | ValueNode;
+
+    export interface AttributeNode extends Literal {
+        type: 'attr';
+        name: string;
+        nameSymbol: SymbolNode;
+        value: ValueNode;
+    }
+
+    export type ValueNode =
+        | CoordNode
+        | SizeNode
+        | AngleNode
+        | NumberNode
+        | SymbolNode;
+
+    export interface CoordNode extends Literal {
+        type: 'coord';
+        value: {
+            x: string;
+            y: string;
+        };
+        valueNode: {
+            x: NumberNode;
+            y: NumberNode;
+        };
+    }
+
+    export interface SizeNode extends Literal {
+        type: 'size';
+        value: {
+            width: string;
+            height: string;
+        };
+        valueNode: {
+            width: NumberNode;
+            height: NumberNode;
+        };
+    }
+
+    export interface AngleNode extends Literal {
+        type: 'angle';
+        value: string;
+        valueNode: NumberNode;
+        unit: string;
+    }
+
+    export interface NumberNode extends Literal {
+        type: 'number';
+        value: string;
+        rawValue: string;
+    }
+
+    export interface SymbolNode extends Literal {
+        type: 'symbol';
+        value: string;
+    }
+
+    export interface CommentNode extends Literal {
+        type: 'comment';
+        value: string;
+    }
+
+    export interface TextNode extends Literal {
+        type: 'text';
+        value: string;
+    }
+
+    export interface XMLNode extends Node {
+        type: 'xml';
+        content: TextNode | CommentNode | ElementNode;
+    }
+
+    export interface ElementNode extends Parent {
+        type: 'element';
+        tagName: string;
+        properties: { [key: string]: string };
+        children: (TextNode | CommentNode | ElementNode)[];
+    }
+}
+
+export function parse(
+    input: string,
+    options?: PEGJs.ParserOptions,
+): AST.StatementValueNode[];
 
 export class SyntaxError extends Error {
     public message: string;
