01e61e1811e3c968a98cf50825d3afef31bdfbac
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-04-21 20:03:33 +0900

    🏷️ PEG.jsが生成するパーサの型情報ファイルを追加
    PEG.jsが生成するパーサ dsl.pegjs.js の型情報ファイルを作成した。

---
+++ src/parser/dsl.pegjs.d.ts
@@ -0,0 +1,66 @@
+interface ParserOptions {
+    startRule?: string;
+}
+
+export interface LiteralExpectation {
+    type: 'literal';
+    text: string;
+    ignoreCase: boolean;
+}
+
+export interface ClassExpectation {
+    type: 'class';
+    parts: ([string, string] | string)[];
+    inverted: boolean;
+    ignoreCase: boolean;
+}
+
+export interface AnyExpectation {
+    type: 'any';
+}
+
+export interface EndExpectation {
+    type: 'end';
+}
+
+export interface OtherExpectation {
+    type: 'other';
+    description: string | unknown;
+    // Note: descriptionパラメータの値は、PEG.jsの実装上はstringを想定しているように思えるが、
+    //       expected()関数の引数にどのような値も渡せるため、
+    //       unknown型も併記した。
+}
+
+export type Expectation =
+    | LiteralExpectation
+    | ClassExpectation
+    | AnyExpectation
+    | EndExpectation
+    | OtherExpectation;
+
+interface Location {
+    start: {
+        offset: number;
+        line: number;
+        column: number;
+    };
+    end: {
+        offset: number;
+        line: number;
+        column: number;
+    };
+}
+
+export function parse(input: string, options?: ParserOptions): unknown;
+
+export class SyntaxError extends Error {
+    public message: string;
+
+    public expected: Expectation[] | null;
+
+    public found: string | null;
+
+    public location: Location;
+
+    public name: 'SyntaxError';
+}
