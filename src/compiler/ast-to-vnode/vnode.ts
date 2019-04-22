f95bcf0491d1b593c97ae73c5da1fc9beda1fcc3
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-04-22 19:45:39 +0900

    ✨ 値がVNodeか否かを判定する関数を追加
    対象の値が仮想DOMノードかどうかを判定する関数を作成し、VNodeの型定義ファイルに追加した。

--- src/compiler/ast-to-vnode/vnode.ts
+++ src/compiler/ast-to-vnode/vnode.ts
@@ -3,3 +3,18 @@ export default interface VNode {
     attributes: { [key: string]: string };
     children: VNode[];
 }
+
+export function isVNode(value: unknown): value is VNode {
+    if (typeof value === 'object' && value !== null) {
+        const obj: { [key: string]: unknown } = value;
+        if (
+            typeof obj.nodeName === 'string' &&
+            typeof obj.attributes === 'object' &&
+            obj.attributes &&
+            Array.isArray(obj.children)
+        ) {
+            return true;
+        }
+    }
+    return false;
+}
