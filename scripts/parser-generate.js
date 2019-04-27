e093ef4906b92e95f05df2f2f0d0ba413bbec7d1
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-04-27 23:54:03 +0900

    ✨ TypeScript用のPEG.jsパーサを生成するスクリプトを追加
    PEG.jsのパーサ定義ファイルから、TypeScript用のパーサを生成するビルド用スクリプトを追加した。

---
+++ scripts/parser-generate.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+
+const makeDir = require('make-dir');
+const pegjs = require('pegjs');
+const tspegjs = require('ts-pegjs');
+
+const customHeaderRegExp = /\/\*:header((?:(?!\*\/)[\s\S])+)\*\//g;
+const returnTypeRegExp = /\/\/:([^\r\n]+)(?:\r\n?|\n)((?![0-9])[a-zA-Z0-9_]+)/g;
+
+const [fsReadFile, fsWriteFile] = [fs.readFile, fs.writeFile].map(
+  util.promisify,
+);
+
+function trimNewlines(str) {
+  return str.replace(/^[\r\n]+|[\r\n]+$/g, '');
+}
+
+async function createFile(filepath, contents) {
+  await makeDir(path.dirname(filepath));
+  await fsWriteFile(filepath, contents);
+}
+
+(async () => {
+  const inputFullpath = path.resolve(process.argv[2]);
+  const outputFullpath =
+    process.argv.length > 3
+      ? path.resolve(process.argv[3])
+      : `${inputFullpath}.ts`;
+
+  const inputContents = await fsReadFile(inputFullpath, 'utf8');
+
+  const customHeaders = [];
+  for (;;) {
+    const matchResult = customHeaderRegExp.exec(inputContents);
+    if (!matchResult) {
+      break;
+    }
+    customHeaders.push(matchResult[1]);
+  }
+  const customHeaderStr = customHeaders
+    .map(trimNewlines)
+    .filter(str => str !== '')
+    .join('\n\n');
+
+  const returnTypes = {};
+  for (;;) {
+    const matchResult = returnTypeRegExp.exec(inputContents);
+    if (!matchResult) {
+      break;
+    }
+
+    const outputType = matchResult[1].trim();
+    const ruleName = matchResult[2];
+    returnTypes[ruleName] = outputType;
+  }
+
+  const options = {
+    output: 'source',
+    plugins: [tspegjs],
+    tspegjs: {
+      customHeader: customHeaderStr !== '' ? `${customHeaderStr}\n` : null,
+    },
+    returnTypes,
+  };
+
+  const parser = pegjs.generate(inputContents, options);
+
+  await createFile(outputFullpath, parser);
+})();
