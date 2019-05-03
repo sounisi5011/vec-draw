a51547c6b0504e0341b120312919763d0df4ed14
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-05-03 23:35:47 +0900

    🚚 空のDSLをコンパイルするテストコードを ./test/compile/empty-input/ 以下に移動
    空の内容のDSLをコンパイルするテストファイルを、empty-inputディレクトリ以下に移動した。

--- test/compile/empty-input/compile.empty-input.ts
+++ test/compile/empty-input/compile.empty-input.ts
@@ -1,17 +1,17 @@
 import test from 'ava';
 import { readFile } from 'fs';
 import { promisify } from 'util';
-import { compile } from '../src';
+import { compile } from '../../../src';
 
 const readFileAsync = promisify(readFile);
 
 test.before(() => {
-    process.chdir('./test/');
+    process.chdir(__dirname);
 });
 
 test('空のSVG', async t => {
-    const srcText = await readFileAsync('assets/empty.vec-draw', 'utf-8');
-    const destText = await readFileAsync('assets/empty.svg', 'utf-8');
+    const srcText = await readFileAsync('empty.vec-draw', 'utf-8');
+    const destText = await readFileAsync('empty.svg', 'utf-8');
 
     t.is(compile(srcText), destText);
 });
