58bf08bac1a0e463e3dabfa9d8edf5b494747dce
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-05-05 01:38:34 +0900

    ✨ PEG.jsのパーサが発したエラーを、SyntaxErrorクラスで包み、再度投げる実装に変更
    PEG.jsのパーサが発したエラーをcatchし、専用のSyntaxErrorクラスで包んで再度throwする実装に変更した。

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,10 +1,22 @@
 import * as parser from './dsl.parser';
 import * as AST from './dsl.type';
+import WrapSyntaxError from '../error/syntax';
 
 export const { SyntaxError } = parser;
 
 export { AST };
 
 export function parse(sourceText: string): AST.RootNode {
-    return parser.parse(sourceText);
+    try {
+        return parser.parse(sourceText);
+    } catch (error) {
+        if (error instanceof parser.SyntaxError) {
+            throw new WrapSyntaxError(
+                error.message,
+                error.location,
+            ).setPrevious(error);
+        } else {
+            throw error;
+        }
+    }
 }
